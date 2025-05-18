012814f7b8086f5cea44d73cdf1b6ea50d2a5064
myutandk <myutan.dk@gmail.com>
2025-05-18 22:00:26 +0900

    コンテスト

---
+++ contest/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+	var N, M = 36, 12
+
+	input := make(map[int]string)
+
+	var max = 0
+
+	for i := N; i > 0; i-- {
+		scanner.Scan()
+		field := strings.Fields(scanner.Text())
+		score, _ := strconv.Atoi(field[1])
+		ex := score / len(field[0])
+		input[ex] = field[0]
+
+		//1文字あたりの期待値高いのを入れる
+		if max < ex {
+			max = ex
+		}
+	}
+
+	length := len(input[max])
+	for index, s := range input[max] {
+		output := [12]int{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+		if index == length-1 {
+			output[0] = 100
+		} else {
+			output[index+1] = 100
+		}
+		fmt.Printf("%c %d %d %d %d %d %d %d %d %d %d %d %d\n", s, output[0], output[1], output[2], output[3], output[4], output[5], output[6], output[7], output[8], output[9], output[10], output[11])
+	}
+
+	if length < M {
+		for i := M - length; i > 0; i-- {
+			output := [12]int{100, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+			fmt.Printf("%c %d %d %d %d %d %d %d %d %d %d %d %d\n", 'a', output[0], output[1], output[2], output[3], output[4], output[5], output[6], output[7], output[8], output[9], output[10], output[11])
+		}
+	}
+
+}
